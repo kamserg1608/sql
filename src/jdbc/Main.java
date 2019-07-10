1774ae19db08e4ed884d3778caa5cff4467804b3
Serega <kamserg1608@gmail.com>
2019-07-10 16:11:05 +0300

    Lesson 186

---
+++ src/jdbc/Main.java
@@ -0,0 +1,16 @@
+package jdbc;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String userName = "postgres";
+        String password = "root";
+        String connectionURl = "jdbc:postgresql://localhost:5432/test";
+        Class.forName("org.postgresql.Driver");
+        try(Connection connection = DriverManager.getConnection(connectionURl, userName, password)){
+            System.out.println("We're connected");
+        }
+
+    }
+}
